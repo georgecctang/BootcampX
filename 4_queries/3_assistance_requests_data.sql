22adef80e2c873fe4634d141c22ff8fec7fc34f1
George Tang <georgecctang@gmail.com>
2020-10-14 22:07:17 +0000

    Set up the JS project

--- 4_queries/3_assistance_requests_data.sql
+++ 4_queries/3_assistance_requests_data.sql
@@ -1,4 +1,4 @@
-SELECT t.name AS teacher, s.name AS student, a.name AS assignment, ar.completed_at - ar.started_at AS duration
+SELECT t.name, s.name AS student, a.name AS assignment, ar.completed_at - ar.started_at AS duration
 FROM assistance_requests AS ar
 JOIN teachers AS t ON ar.teacher_id = t.id
 JOIN students AS s ON ar.student_id = s.id
